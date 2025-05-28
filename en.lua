747a25df7f46d8e5eac46f4193fdc850d0db9311
DrAgOnLeGeNd66 <58800751+DrAgOnLeGeNd66@users.noreply.github.com>
2025-05-28 11:03:06 +0200

    Add files via upload

---
+++ en.lua
@@ -0,0 +1,20 @@
+return {
+    descriptions = {
+        Joker = {
+            j_vir_virus = {
+                name = "Virus",
+                text = {
+                    "Every {C:attention}20{} discards",
+                    "spread the {C:dark_edition}Virus{}",
+                    "{C:inactive}(Makes random joker negative){}",
+                    "{C:inactive}(#1# discards left){}"
+                }
+            }
+        }
+    },
+    misc = {
+        dictionary = {
+            k_virus = "Virus"
+        }
+    }
+}
